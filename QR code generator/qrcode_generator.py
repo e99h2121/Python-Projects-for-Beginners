e39cf9be1fdf6cd44afdfd265f88baa1470dc01b
e99h2121 <yamada.nobuk@gmail.com>
2021-03-28 03:49:35 +0900

    updated qrcode generator and image

--- QR code generator/qrcode_generator.py
+++ QR code generator/qrcode_generator.py
@@ -7,4 +7,6 @@ data = input("Enter the text or link to generate QR code: ")
 qr = pyqrcode.create(data)
 
 # Using .svg method to save the qr code as SVG file of provided name & scale 
-qr.svg('qr_code.svg', scale = 8)
+# qr.svg('qr_code.svg', scale = 8)
+# Using .png method to save the qr code as SVG file of provided name & scale 
+qr.png('qr_code.png', scale = 8)
