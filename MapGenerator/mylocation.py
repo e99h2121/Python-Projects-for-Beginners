654d27a7129386a559a822c6cfefe686fe7beeaa
e99h2121 <yamada.nobuk@gmail.com>
2021-04-18 05:40:30 +0900

    upgraded

--- MapGenerator/mylocation.py
+++ MapGenerator/mylocation.py
@@ -1,4 +1,18 @@
-import folium
+## https://qiita.com/shira11/items/58a186fde9d22a88b3d3
+## https://qiita.com/pork_steak/items/f551fa09794831100faa
 
-map = folium.Map(location=[35.3, 139.9])
+import folium
+from geopy.geocoders import Nominatim
+
+location = input("場所を入力:")
+copyright_osm = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+
+geolocator = Nominatim(user_agent="test")
+location = geolocator.geocode(location)
+lat, long = location.latitude, location.longitude
+print("full address = ", location.address)
+
+map = folium.Map(location=[lat, long],
+  attr=copyright_osm,
+)
 map.save("mylocation.html")
